bb074af9f1a78b51d459fff399ea8daefdcbb647
Kendell Pierre <kendell.pierre@randelllamontgroup.com>
2025-07-10 23:01:20 +0000

    it didnt solve the error. check the codebase and see where else the erro

--- src/app/(splash)/layout.tsx
+++
@@ -1,2 +0,0 @@
-// This file is no longer needed as its logic has been moved to /src/app/layout.tsx
-// You can safely delete this file.
--- src/app/(splash)/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file is no longer needed as its content has been moved to /src/app/page.tsx
-// You can safely delete this file.
